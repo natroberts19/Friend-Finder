0df16be3cfb4e3ea1e31833593ee60c569bc94d0
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-28 12:31:05 -0500

    route files

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// This file will store the array of objects for each greyhound ("friend").
+// Will hold all client side js also?
+
+var friends = [
+{
+    "name": "Peachy Keen",
+    "photo": "../public/images/grey1.jpg",
+    "scores": [
+        2,
+        3,
+        5,
+        1,
+        3,
+        4,
+        1,
+        4,
+        5,
+        5
+    ]
+},
+{
+    "name": "Happy Harvey",
+    "photo": "../public/images/grey2.jpg",
+    "scores": [
+        4,
+        1,
+        2,
+        3,
+        1,
+        5,
+        1,
+        2,
+        4,
+        4
+    ]
+},
+{
+    "name": "Santa's Little Helper",
+    "photo": "../public/images/grey3.jpg",
+    "scores": [
+        5,
+        3,
+        1,
+        1,
+        2,
+        5,
+        2,
+        3,
+        5,
+        1
+    ]
+},
+{
+    "name": "Beach Bum",
+    "photo": "../public/images/grey4.jpg",
+    "scores": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1
+    ]
+},
+{
+    "name": "Double Trouble",
+    "photo": "../public/images/grey-double.jpg",
+    "scores": [
+        5,
+        5,
+        4,
+        4,
+        3,
+        3,
+        2,
+        1,
+        2,
+        1
+    ]
+},
+]
