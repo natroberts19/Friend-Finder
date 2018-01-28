ec1b7fa3084061764583169f36b691ce3cd3e5a9
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-28 11:39:48 -0500

    Added file and folder structure, set up front-end html and css pages

---
+++ app/public/javascript/survey.js
@@ -0,0 +1,5 @@
+// This javascript file will hold all the front-end (client) javascript functionality for both home.html and survey.html.
+
+// Home.html will need a click function for the Take the Survey button.
+
+// Survey.html will need a click function to Submit the survey data and to run the modal actions.
